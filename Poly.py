d723439e7696a0f6c7e742fd878a2c50f106b007
Thahaseen-Shaik <thahaseengulamshaik@gmail.com>
2025-07-10 13:47:49 +0000

    class and objects

---
+++ Poly.py
@@ -0,0 +1,12 @@
+#Create Cat and Dog classes. Both should have a make_sound() method that prints different sounds.
+class Poly:
+    def navigate(self):
+        pass
+class Cat(Poly):
+    def navigate(self):
+        print("cats sounds meow meow")
+class Dog(Poly):
+    def navigate(selfself):
+        print("Dog sounds bow bow")
+for c in[Cat(), Dog()]:
+    c.navigate()
